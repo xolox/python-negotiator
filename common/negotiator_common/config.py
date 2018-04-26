af4677ca46f82ed4486986f180eb8d8c3db6a0d5
Peter Odding <peter@peterodding.com>
2018-04-26 21:11:21 +0200

    Rewrite documentation on configuration defaults

--- common/negotiator_common/config.py
+++ common/negotiator_common/config.py
@@ -1,46 +1,38 @@
 # Scriptable KVM/QEMU guest agent in Python.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: October 24, 2014
+# Last Change: April 26, 2018
 # URL: https://negotiator.readthedocs.org
 
 """
 ``negotiator_common.config`` - Configuration defaults
 =====================================================
-
-.. data:: CHANNELS_DIRECTORY
-
-   The pathname of the directory containing the host side of each channel.
-   Defaults to ``/var/lib/libvirt/qemu/channel/target``.
-
-.. data:: USER_COMMANDS_DIRECTORY
-
-   The pathname of the directory containing user defined commands that 'the
-   other side' can invoke through Negotiator. Defaults to
-   ``/usr/lib/negotiator/commands``.
-
-.. data:: DEFAULT_TIMEOUT
-
-   The number of seconds to wait for a reply from the other side. If more time
-   elapses an exception is raised causing the process to exit with a nonzero
-   status code. Defaults to 10 seconds.
-
-.. data:: GUEST_TO_HOST_CHANNEL_NAME
-
-   The name of the channel that's used for communication initiated by the
-   guest. Defaults to ``negotiator-guest-to-host.0``.
-
-.. data:: HOST_TO_GUEST_CHANNEL_NAME
-
-   The name of the channel that's used for communication initiated by the
-   host. Defaults to ``negotiator-host-to-guest.0``.
 """
 
 import os
 
 CHANNELS_DIRECTORY = '/var/lib/libvirt/qemu/channel/target'
+"""The pathname of the directory containing the host side of each channel."""
+
 USER_COMMANDS_DIRECTORY = '/usr/lib/negotiator/commands'
+"""
+The pathname of the directory containing user defined commands
+that 'the other side' can invoke through `negotiator`.
+"""
+
 BUILTIN_COMMANDS_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'scripts')
+"""The directory with built-in commands (a string)."""
+
 GUEST_TO_HOST_CHANNEL_NAME = 'negotiator-guest-to-host.0'
+"""The name of the channel that's used for communication initiated by the guest (a string)."""
+
 HOST_TO_GUEST_CHANNEL_NAME = 'negotiator-host-to-guest.0'
+"""The name of the channel that's used for communication initiated by the host (a string)."""
+
 DEFAULT_TIMEOUT = 10
+"""
+The number of seconds to wait for a reply from the other side (an integer).
+
+If more time elapses an exception is raised causing the process to exit with a
+nonzero status code.
+"""
