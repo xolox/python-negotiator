4cba0ba4f0229a521349d9258e30b53093be76d9
Peter Odding <peter@peterodding.com>
2018-04-26 21:09:13 +0200

    Move compact() to humanfriendly

--- common/negotiator_common/utils.py
+++ common/negotiator_common/utils.py
@@ -9,22 +9,11 @@
 =====================================================
 """
 
+# Standard library modules.
 import signal
 
-
-def compact(text, **kw):
-    """
-    Compact whitespace and format any arguments into the given string.
-
-    Trims leading and trailing whitespace, replaces runs of whitespace with a
-    single space and formats any keyword arguments into the resulting string
-    using :py:func:`str.format()`.
-
-    :param text: The text to compact (a string).
-    :param kw: Any keyword arguments to apply using :py:func:`str.format()`.
-    :returns: The compacted, formatted string.
-    """
-    return ' '.join(text.split()).format(**kw)
+# External dependencies.
+from humanfriendly.text import compact
 
 
 def format_call(function, *args, **kw):
